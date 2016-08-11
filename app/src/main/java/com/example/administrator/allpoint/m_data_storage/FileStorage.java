fb173be285986a08352bce75ebd07aa45c9475a3
renfei <778732662@qq.com>
2016-08-12 07:16:24 +0800

    2016/8/12 7:16

---
+++ app/src/main/java/com/example/administrator/allpoint/m_data_storage/FileStorage.java
@@ -0,0 +1,11 @@
+package com.example.administrator.allpoint.m_data_storage;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huahua on 2016/8/11.
+ * 文件存储
+ */
+public class FileStorage extends AppCompatActivity{
+
+}
